9cfdac6b8c83b50d518a9691e1d1f788146070d2
César Hernández <cesarinhernndezyes@gmail.com>
2024-03-15 23:27:35 -0600

    Se añadió react al proyecto para meter interactvidad y se cre+o Proyects.jsx para sustituir el Projects.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+        'nebuverse': "url('https://axeldevport.netlify.app/Project-1.webp')",
+        'evergreen': "url('/evergreen.webp')",
+			},
 			fontFamily: {
 				'onest' : ['Onest Variable', 'sans-serif']
 			}
