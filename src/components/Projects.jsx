b61a2d5b0e694fc7938a075cfc46cda7e6c5a174
TejocoteChamba <cesarhernndezyes@gmail.com>
2024-03-16 13:21:28 -0600

    Mejoré el About pero no está terminado

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,19 +10,17 @@ function Projects() {
   let toggleClassCheck = btnState ? " active" : null;
 
   return (
-    <div className="row-span-3 col-span-2 gap-4 md:col-span-4 lg:row-span-6 lg:col-span-4 bg-[#174793] text-white flex items-center rounded-2xl overflow-hidden ">
+    <button onClick={handleClick} className="row-span-3 col-span-2 gap-4 md:col-span-4 lg:row-span-6 lg:col-span-4 bg-[#174793] text-white flex items-center rounded-2xl overflow-hidden ">
       <div className="bg-[#174793] w-full h-full rounded-2xl flex flex-col items-center group ">
-        <button className="" onClick={handleClick}>
-          <h3 className="inset-0 text text-2xl font-bold"> Proyectos</h3>
-        </button>
-        <div className="flex w-full h-full lg:flex-col lg:rounded-2xl text-center pt-12 rounded-2xl">
+          <h3 className="text text-2xl my-2 font-bold"> Proyectos</h3>
+        <div className="flex w-full h-full lg:flex-col lg:rounded-2xl text-center pt-2 rounded-2xl">
           <div className="bg-green-400 bg-cover w-full h-full flex items-center justify-center overflow-hidden">
             <h4 className="m-auto font-onest text-xl font-bold text-white rounded-lg bg-green-700 px-4 py-2">
               Evergreen
             </h4>
           </div>
           <div className="bg-purple-400 bg-cover w-full h-full flex items-center justify-center overflow-hidden">
-            <h4 class="m-auto font-onest text-xl font-bold text-white bg-purple-700 px-4 py-2  rounded-lg">
+            <h4 className="m-auto font-onest text-xl font-bold text-white bg-purple-700 px-4 py-2  rounded-lg">
               Nebuverse
             </h4>
           </div>
@@ -33,7 +31,7 @@ function Projects() {
           </div>
         </div>
       </div>
-    </div>
+    </button>
   );
 }
 
