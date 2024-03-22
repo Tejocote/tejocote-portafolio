5f535b3d119e897627f107e5e221380492763fab
César Hernández <cesarinhernndezyes@gmail.com>
2024-03-21 21:45:02 -0600

    nuevas animacioines

--- src/components/Pruebas.jsx
+++ src/components/Pruebas.jsx
@@ -12,32 +12,33 @@ export function Pruebas() {
   }
   const [btnState, setBtnState] = useState(false);
 
-  let toggleClassCheck = btnState ? "delay-200 w-full h-full" : "text-white w-4/6 h-2/6 ";
-  let notProjects = btnState ? "duration-300 transition-all opacity-0 h-0" : "duration-300 transition-all opacity-100";
+  let toggleClassCheck = btnState ? "w-full h-full" : "text-white h-2/6 w-4/6";
+  let notProjects = btnState ? "translatex-4 opacity-0" : " opacity-100";
+  let about = btnState ? "h-0 opacity-0" : "";
 
   return (
     <section className="h-[700px] px-2 mb-8">
-      <div className="h-full flex flex-wrap justify-center">
-        <div className={`w-full h-1/3 p-2 ${notProjects}`}>
+      <div className="h-full flex flex-wrap">
+        <div className={`w-full h-2/6 duration-200 transition-all p-2 ${notProjects} ${about}`}>
         <About/>
         </div>
         <div onClick={handleClick}
-         className={` transition-all p-2 ${toggleClassCheck}`}>
+         className={` transition-all p-2  ${toggleClassCheck}`}>
         <Projects/>
         </div>
         <div>
           
         </div>
-        <div className={`w-2/6 h-2/6 p-2 ${notProjects}`}>
+        <div className={`w-2/6 h-2/6 duration-200 transition-all p-2  ${notProjects}`}>
         <Redes/>
         </div>
-        <div className={`w-2/6 h-1/6 p-2 ${notProjects}`}>
+        <div className={`w-2/6 h-1/6 duration-200 transition-all p-2  ${notProjects}`}>
         <Theme/>
         </div>
-        <div className={`w-4/6 h-1/6 p-2 ${notProjects}`}>
+        <div className={`w-4/6 h-1/6 duration-200 transition-all p-2  ${notProjects}`}>
         <Theme/>
         </div>
-        <div className={`h-1/6 w-full p-2 ${notProjects}`}>
+        <div className={`h-1/6 w-full duration-200 transition-all p-2  ${notProjects}`}>
         <Skills/>
         </div>
        
