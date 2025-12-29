08747616b9a7617fe504f66b9653e18137aaeaae
Your Nam <cesarhernndezyes@gmail.com>
2025-12-29 04:28:21 -0600

    actualizando imágenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()]
