17e2fc5e8c1c3e9d10ca1f4990012b0c3b5ec7c7
Your Nam <cesarhernndezyes@gmail.com>
2026-01-02 16:23:41 -0600

    Cambio de tamaño de letra y descripciones de cada página de los proyectos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,28 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-        'satoshi': ['Satoshi', "Roboto"]
-      },
-		
-			animation: {
-        'infinite-scroll': 'infinite-scroll 8s linear infinite',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 8s linear infinite",
       },
       keyframes: {
-        'infinite-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
-		},
-    colors: {
-        'primary-blue': '#0F172A',   // Fondo
-        'secondary-blue': '#283e70', // Cards
-        'text-main': '#F5EFE6',      // Títulos
-        'text-muted': '#E8DFCA',    // Párrafos
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
       },
-		},
-	plugins: [require('tailwindcss-animated')],
-}
-}
+      colors: {
+        "primary-blue": "#0F172A", // Dark Blue
+        "secondary-blue": "#283e70", // Medium Blue
+      },
+    },
+    plugins: [require("tailwindcss-animated")],
+  },
+};
